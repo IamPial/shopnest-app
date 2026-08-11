67be7c830a5b393a68bebf3fa9d18934e683e52d
IamPial <pialname394@gmail.com>
2026-08-12 01:59:02 +0600

    verify the token

--- server/src/services/products.ts
+++ server/src/services/products.ts
@@ -1,11 +1,12 @@
 import { Router, type Request , type Response } from "express";
 import prisma from "../lib/prisma";
+import verifyToken from "../middlewares/auth";
 
 const router = Router()
 
 
 //for create products data
-router.post('/', async(req:Request, res:Response)=>{
+router.post('/', verifyToken, async(req:Request, res:Response)=>{
    try{
      const productData = req.body
     const createData = await prisma.products.create({
@@ -77,7 +78,7 @@ router.get("/:id", async(req:Request, res:Response)=>{
 
 
 //for delete products data
-router.delete("/:id", async(req:Request, res:Response)=>{
+router.delete("/:id", verifyToken, async(req:Request, res:Response)=>{
     try{
         const id = req.params.id as string
         const data = await prisma.products.delete({
