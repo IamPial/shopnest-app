d161cb4f8d947910da9e0fd5cc7b8ecbaf7f1b2f
IamPial <pialname394@gmail.com>
2026-08-11 11:39:49 +0600

    configure the prisma client

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,18 @@
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const db_connect = process.env.DATABASE_URL;
+if(!db_connect){
+    throw new Error("Database url not found!")
+}
+
+
+const adapter = new PrismaPg({
+  connectionString: db_connect,
+});
+
+export const prisma = new PrismaClient({ adapter });
