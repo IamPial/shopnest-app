dc62577f9756f8240952836c9cc4a83aaa8cb006
IamPial <pialname394@gmail.com>
2026-08-12 00:45:50 +0600

    create login routes

--- server/src/services/auth.services.ts
+++ server/src/services/auth.services.ts
@@ -1,5 +1,6 @@
 import { Router, type Request, type Response } from "express";
 import bcrypt from "bcrypt"
+import jwt ,{type SignOptions}from 'jsonwebtoken'
 import prisma from "../lib/prisma";
 
 const router = Router()
@@ -42,3 +43,51 @@ router.post("/", async(req:Request, res:Response)=>{
         })
     }
 })
+
+
+//for login
+router.post('/login', async(req:Request, res:Response)=>{
+    try{
+        const {email, password} = req.body
+        const user = await prisma.user.findUnique({where:{email}})
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"Invalid email or password"
+            })
+        }
+      
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.status(401).json({
+                success:false,
+                message:"Invalid email or password"
+            })
+        }
+
+
+        const options: SignOptions = {
+        expiresIn: (process.env.JWT_EXPIRES_IN || "7d") as SignOptions["expiresIn"]
+      };
+      
+        const token = jwt.sign(
+            {id:user.id, email:user.email, role:user.role},
+            process.env.JWT_SECRET as string,
+             options
+        ) 
+        res.json({
+            success:true,
+            message:"login successful",
+            data:{token}
+        })
+
+    }catch(error:any){
+      res.status(500).json({
+        success:false,
+        message:"login failed",
+        data: error
+      })
+    }
+})
+
+export default router
