c0c8f2f792fd9c300e67f5fbb00af0f265cd58dd
IamPial <pialname394@gmail.com>
2026-08-12 01:57:53 +0600

    create users services

---
+++ server/src/services/users.ts
@@ -0,0 +1,36 @@
+import { Router, type Request, type Response } from "express";
+
+import prisma from "../lib/prisma";
+import verifyToken, { isAdmin } from "../middlewares/auth";
+
+const router = Router();
+
+
+//for admin profile
+router.get('/', verifyToken, isAdmin, async (req: Request, res: Response) => {
+    try {
+        const users = await prisma.user.findMany({
+            select: { id: true, name: true, email: true, role: true, createdAt: true }
+      
+        });
+        res.json({ success: true, data: users });
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: "Failed to fetch users", data: error });
+    }
+});
+
+
+//for just my own profile
+router.get('/myself', verifyToken, async (req: any, res: Response) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: req.user.id },
+            select: { id: true, name: true, email: true, role: true }
+        });
+        res.json({ success: true, data: user });
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: "Failed to fetch profile", data: error });
+    }
+});
+
+export default router;
