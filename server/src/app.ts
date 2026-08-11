79cfe536ed9f1f9fc4c0b7f74b9f19281e1f75f0
IamPial <pialname394@gmail.com>
2026-08-11 14:55:01 +0600

    create api routes in this file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,21 @@
 import express, {type Request, type Response} from 'express'
 import cors from 'cors'
+import routes from './routes'
 const app = express()
 
+
+//Middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+
+
+//API Routes
+app.use('/api/v1', routes)
+
+
+//Home Route
 app.get("/", (req:Request, res:Response)=>{
     res.json({
         success:true,
