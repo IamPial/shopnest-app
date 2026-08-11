9e62fcabc54f13a8853a5a5c097f7260c5a39639
IamPial <pialname394@gmail.com>
2026-08-11 14:56:43 +0600

    create post method for categories

---
+++ server/src/services/categories.ts
@@ -0,0 +1,26 @@
+import { Router, type Request , type Response } from "express";
+import prisma from "../lib/prisma";
+
+const router = Router()
+
+router.post('/', async(req:Request, res:Response)=>{
+    try{
+        const categoriesData = req.body;
+        const data = await prisma.category.create({data:categoriesData})
+
+        res.json({
+        success:true,
+        message:"category created successfully",
+        data,
+       })
+    }catch(error:any){
+ 
+    res.json({
+        success:false,
+        message:"failed to create category",
+        data:error,
+    })
+    }
+})
+
+export default router
