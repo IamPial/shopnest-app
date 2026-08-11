64cdfa9c27ff9b838cad001c17b17cf365dfa3e0
IamPial <pialname394@gmail.com>
2026-08-12 02:00:33 +0600

    render users & auth api here

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
 import products from '../services/products'
 import category from "../services/categories"
+import users from "../services/users"
 import authRoutes from "../services/auth.services"
 
 
 const router = Router()
 
 
-//for users
+
+
+
+//for authentication
 router.use("/auth", authRoutes)
+
+//for users
+router.use("/users", users)
+
 
 //for products
 router.use('/products', products)
