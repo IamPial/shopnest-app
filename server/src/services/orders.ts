e67ec2d8937cba58b882d3f937a3ed2e0f0417fb
IamPial <pialname394@gmail.com>
2026-08-12 08:26:30 +0600

    create order services

--- server/src/services/orders.ts
+++ server/src/services/orders.ts
@@ -1,8 +1,27 @@
 import { Router , Request, Response } from "express";
+import prisma from "../lib/prisma";
 
 const router = Router()
 
 
 router.post("/", async(req:Request, res:Response)=>{
-  
+   const {userId, orderItem} = req.body
+
+   if(!userId){
+    return res.status(400).json({
+        success:false,
+        message:"user id not found!"
+    })
+   }
+
+   //find user
+   const user = await prisma.user.findUnique({where:{id:userId}})
+   if(!user){
+    return res.status(404).json({
+        success:false,
+        message:"User not found"
+    })
+   }
+
+
 })
